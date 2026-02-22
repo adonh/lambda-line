ede5d64c4f1b30a154ed5e71d68d19030f350398
adon <dev@adon.dev>
2026-02-22 02:16:53 -0500

    defer to :prefix-symbol to allow easier user customizations

--- lambda-line.el
+++ lambda-line.el
@@ -1361,7 +1361,7 @@ STATUS, NAME, PRIMARY, and SECONDARY are always displayed. TERTIARY is displayed
 
 (defun lambda-line-term-mode (mode-format)
   (lambda-line-compose mode-format
-                       " >_ "
+                       (plist-get (cdr mode-format) :prefix-symbol)
                        (plist-get (cdr mode-format) :name)
                        (concat lambda-line-display-group-start
                                (file-name-nondirectory shell-file-name)
@@ -1384,7 +1384,7 @@ STATUS, NAME, PRIMARY, and SECONDARY are always displayed. TERTIARY is displayed
 
 (defun lambda-line-ssh-mode (mode-format)
   (lambda-line-compose mode-format
-                       " >_ "
+                       (or (plist-get (cdr mode-format) :prefix-symbol) " >_ ")
                        "Terminal"
                        (concat lambda-line-display-group-start
                                (lambda-line-get-ssh-host default-directory)
@@ -1401,7 +1401,7 @@ STATUS, NAME, PRIMARY, and SECONDARY are always displayed. TERTIARY is displayed
 
 (defun lambda-line-shell-mode (mode-format)
   (lambda-line-compose mode-format
-                       " >_ "
+                       (plist-get (cdr mode-format) :prefix-symbol)
                        (plist-get (cdr mode-format) :name)
                        (concat lambda-line-display-group-start
                                (buffer-name)
